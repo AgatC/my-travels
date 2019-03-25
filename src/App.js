b95d0dc90b62305fc55b35a18bdd80a455dc95a8
Agathe Cazals <agathe.cazals@gmail.com>
2019-03-25 11:08:22 +0100

    First

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Travel from "./Travel";
 
 class App extends Component {
   render() {
@@ -8,18 +10,20 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">My Travel</h1>
         </header>
+        <Travel
+          destination=<h1>Montreal</h1>
+          country="Canada"
+          photo="https://cdn.pixabay.com/photo/2012/02/28/00/49/squirrel-17854_960_720.jpg"
+	  distance = "5717 km"
+        />
+        <Travel
+          destination=<h1>Cinque Terre</h1>
+          country="Italy"
+          photo="https://cdn.pixabay.com/photo/2015/03/10/00/09/cinque-terre-666601_960_720.jpg"
+	  distance = "847 km"
+        />
       </div>
     );
   }
